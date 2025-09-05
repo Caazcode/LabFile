61b065b15d96634cd8b6c7f21613691b0e49fb16
caazcode <caaz5575@gmail.com>
2025-09-05 11:20:04 -0600

    Arreglo

--- src/ManejadorArchivos.java
+++ src/ManejadorArchivos.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author saidn
- */
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.text.*;
